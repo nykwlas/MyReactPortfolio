7f957b32422ad6468af7415cdef5d4087a657aac
nykwlas <nsolom03@cs.ucy.ac.cy>
2020-04-15 15:21:09 +0300

    My Portfolio

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# MyReactPortfolio
-Portfolio website build with React
+Change the values in src/resumeData.js to suit your use-case.
+Run npm start to spin the up the local dev server port 3000.(http://localhost:3000).
+
---
+++ src/resumeData.js
@@ -0,0 +1,97 @@
+let resumeData = {
+  imagebaseurl: "",
+  name: "Nicolas Solomonides",
+  role: "Software Developer",
+  email: "nykwlas@gmail.com",
+  skypeid: "nicolas solomonidis",
+  roleDescription:
+    " I like to work most of my time learning new technologies to further develop and use my skills or simply play games in my free time.",
+  socialLinks: [
+    {
+      name: "linkedin",
+      url: "https://www.linkedin.com/in/nicolas-solomonides/",
+      className: "fa fa-linkedin",
+    },
+    {
+      name: "github",
+      url: "https://github.com/nykwlas",
+      className: "fa fa-github",
+    },
+    {
+      name: "twitter",
+      url: "https://twitter.com/Nicolas08968019",
+      className: "fa fa-twitter",
+    },
+  ],
+  aboutme:
+    "I'm in my fourth year of studies towards a bachelor's in Computer Science at the University of Cyprus. I have a general grade of 8.07/10 in my courses and I have developed excellent programming and teamwork skills through the group projects and homework exercises. Also I worked as an intern and gained professional experience in my field as well as valuable contacts. Now I am looking for a job as a Software Engineer, to further develop and use my skillsthrough a training agreement in computer science sector.",
+  address: "Cyprus",
+  website: "nykwlas@gmail.com",
+  education: [
+    {
+      UniversityName: "University of Cyprus",
+      specialization: "Computer Science",
+      MonthOfPassing: "May",
+      YearOfPassing: "2020",
+      Achievements: "4 years studying at UCY for my bachelor's degree",
+    },
+    {
+      UniversityName: "Military service",
+      specialization: "Infantry Sergeant",
+      MonthOfPassing: "June",
+      YearOfPassing: "2016",
+      Achievements: "2 years of military service at Military base of Nisou, Nicosia",
+    },
+  ],
+  work: [
+    {
+      CompanyName: "KPMG",
+      specialization: "Internship",
+      MonthOfLeaving: "June",
+      YearOfLeaving: "2019",
+      Achievements: "Fisheries project of DFMR",
+    },
+    {
+      CompanyName: "Enactia Ltd",
+      specialization: "Internship",
+      MonthOfLeaving: "July",
+      YearOfLeaving: "2019",
+      Achievements: "Summer Placement program of ucy",
+    },
+  ],
+  skillsDescription: "HTML/CSS | Bootstrap | JavaScript | ReactJS | React Native | Firebase | Oracle CI",
+  skills: [
+    {
+      skillname: "reactjs",
+    },
+    {
+      skillname: "reactnative",
+    },
+    {
+      skillname: "firebase",
+    },
+  ],
+  portfolio: [
+    {
+      name: "Fisheries",
+      description: "Mobile App",
+      imgurl: "images/portfolio/fisheries.png",
+    },
+  ],
+  testimonials: [
+    {
+      description: "The most powerful tool we have as developers is automation.",
+      name: "Scott Hanselman",
+    },
+    {
+      description: "Technology is just a tool. In terms of getting the kids working together and motivating them, the teacher is the most important.",
+      name: "Bill Gates",
+    },
+    {
+      description: "Programming is not a zero-sum game. Teaching something to a fellow programmer doesn’t take it away from you.",
+      name: "John Carmack",
+    },
+  ],
+};
+
+export default resumeData;
